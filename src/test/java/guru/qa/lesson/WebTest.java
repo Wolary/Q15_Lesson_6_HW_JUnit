eaa2d2c1784073e1c24e9092ef2d39ec54327381
Nemond <koloss200@gmail.com>
2022-10-28 15:17:01 +0300

    Csv Parametrized Test Added

--- src/test/java/guru/qa/lesson/WebTest.java
+++ src/test/java/guru/qa/lesson/WebTest.java
@@ -3,6 +3,7 @@ package guru.qa.lesson;
 import com.codeborne.selenide.CollectionCondition;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Condition.text;
@@ -12,9 +13,9 @@ public class WebTest {
 
     //ТЕСТОВЫЕ ДАННЫЕ ["Selenide", "JUnit"]
 
-    @ValueSource(strings = {"Selenide", "JUnit"})
-    @ParameterizedTest (name = "Проверка числа результатов поиска в яндексе для запроса {0}")
-    //[test_data] == (String Data)
+    @ValueSource(strings = {"Selenide", "JUnit"}) //работает только со стрингами и примитивными типами данных
+    @ParameterizedTest(name = "Проверка числа результатов поиска в яндексе для запроса {0}")
+        //[test_data] == (String Data)
     void yaSearchCommonTest(String testData) {
         open("https://ya.ru");
         $("#text").setValue(testData);
@@ -23,6 +24,24 @@ public class WebTest {
                 .shouldHave(CollectionCondition.size(10))
                 .first()
                 .shouldHave(text(testData));
+    }
+
+
+    @CsvSource(value = {
+            "Selenide | - Selenide - это фреймворк для автоматизированного тестирования",
+            "JUnit | - JUnit is a unit testing framework for the Java"
+    },
+            delimiter = '|') // вводим разделитель по умолчанию запятая
+
+    @ParameterizedTest(name = "Проверка числа результатов поиска в яндексе для запроса {0}")
+    void yaSearchCommonTestDifferentExpectedText(String searchQuery, String expectedText) {
+        open("https://ya.ru");
+        $("#text").setValue(searchQuery);
+        $("button[type='submit']").click();
+        $$("li.serp-item")
+                .shouldHave(CollectionCondition.size(10))
+                .first()
+                .shouldHave(text(expectedText));
     }
 
     // тесты до параметризированного
