4c038ead00dcba58ee27acaf7c32f3e1300d7d6c
Nemond <koloss200@gmail.com>
2022-10-28 16:01:53 +0300

    Stream Parametrized Test Added

---
+++ src/test/java/guru/qa/lesson/Data/Locale.java
@@ -0,0 +1,5 @@
+package guru.qa.lesson.Data;
+
+public enum Locale {
+    EN, RU
+}
--- src/test/java/guru/qa/lesson/WebTest.java
+++ src/test/java/guru/qa/lesson/WebTest.java
@@ -1,13 +1,23 @@
 package guru.qa.lesson;
 
 import com.codeborne.selenide.CollectionCondition;
-import org.junit.jupiter.api.Test;
+import guru.qa.lesson.Data.Locale;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
+
 
 public class WebTest {
 
@@ -26,7 +36,6 @@ public class WebTest {
                 .shouldHave(text(testData));
     }
 
-
     @CsvSource(value = {
             "Selenide | - Selenide - это фреймворк для автоматизированного тестирования",
             "JUnit | - JUnit is a unit testing framework for the Java"
@@ -43,6 +52,32 @@ public class WebTest {
                 .first()
                 .shouldHave(text(expectedText));
     }
+
+
+    static Stream<Arguments> selenideSiteButtonTest() {
+        return Stream.of(
+                Arguments.of(List.of("Quick start", "Docs", "FAQ", "Blog", "Javadoc", "Users", "Quotes"), Locale.EN),
+                Arguments.of(List.of("С чего начать?", "Док", "ЧАВО", "Блог", "Javadoc", "Пользователи", "Отзывы"), Locale.RU)
+        );
+    }
+
+    @MethodSource()
+    @ParameterizedTest(name ="проверка отображения кнопок для локали : {1}")
+    void selenideSiteButtonTest(List<String> buttonsTest, Locale locale) {
+        open("https://selenide.org");
+        $$("#languages a").find(text(locale.name())).click();
+        $$(".main-menu-pages a").filter(visible)
+                .shouldHave(CollectionCondition.texts(buttonsTest));
+    }
+
+    @EnumSource(Locale.class)
+    @ParameterizedTest
+    void checkLocal(Locale locale){
+        open("https://selenide.org");
+        $$("#languages a").find(text(locale.name())).shouldHave(text(locale));
+    }
+
+
 
     // тесты до параметризированного
 //    @Test
